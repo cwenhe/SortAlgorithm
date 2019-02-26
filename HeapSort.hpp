3c3f23ae0d3ce78c8697edf9f3f615c0b840f921
cwenhe <31620820+cwenhe@users.noreply.github.com>
2019-02-26 14:01:45 +0800

    Add files via upload

---
+++ HeapSort.hpp
@@ -0,0 +1,48 @@
+﻿#pragma once
+#include <algorithm>
+
+
+namespace heap 
+{
+
+	//大顶堆
+	template<typename _Value>
+	void heapAdjust(_Value* arr, size_t cur_node, size_t const end)
+	{
+		auto left_child = 2 * cur_node+1;
+
+		while (end > left_child)
+		{
+			auto check_child = left_child;
+			if (end > left_child + 1 && arr[left_child + 1] > arr[left_child])
+				check_child = left_child + 1;
+
+			if (arr[check_child] > arr[cur_node])
+				std::swap(arr[check_child], arr[cur_node]);
+			else
+				break;
+
+			cur_node = check_child;
+			left_child = 2 * cur_node+1;
+		}
+	}
+
+	template< typename _Value>
+	void makeHeap(_Value* arr, size_t const len)
+	{
+		if (0 == len)
+			return;
+
+		for (size_t cur_node = len; 0 != cur_node; --cur_node)
+		{
+			heapAdjust(arr, cur_node-1, len);
+		}
+
+
+		for (size_t max_node = len - 1; 0 != max_node; --max_node)
+		{
+			std::swap(arr[0], arr[max_node]);
+			heapAdjust(arr, 0, max_node);
+		}
+	}
+}
