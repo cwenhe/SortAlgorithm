f42dad6960c11a5c78a4badef2982ef3ef7b9c1d
cwenhe <wenhe.chen@youhualin.com>
2019-03-02 17:07:10 +0800

    add InsertionSort

---
+++ algorithm/Insertion.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <iterator>
+
+namespace sort
+{
+	template<typename _Iterator, typename _Pred = std::less< std::iterator_traits<_Iterator>::value_type> >
+	void insertion(_Iterator begin, _Iterator end, _Pred const& pred = _Pred())
+	{
+		auto r_end = std::reverse_iterator<_Iterator>(begin);
+		for (auto iter = std::next(begin); end != iter; ++iter)
+		{
+			auto r_it = std::reverse_iterator<_Iterator>(std::next(iter));
+			for (auto sub_iter = std::next(r_it); r_end != sub_iter; r_it = sub_iter++)
+			{
+				if (pred(*r_it, *sub_iter))
+					std::swap(*r_it, *sub_iter);
+				else
+					break;
+			}
+
+		}
+
+	}
+
+	template<typename _Container, typename _Pred = std::less< _Container::value_type > >
+	void insertion(_Container& values, _Pred const& pred = _Pred())
+	{
+		insertion(values.begin(), values.end(), pred);
+	}
+
+}
--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 #include "algorithm/HeapSort.hpp"
 #include "algorithm/BubbleSort.hpp"
 #include "algorithm/SelectionSort.hpp"
+#include "algorithm/Insertion.hpp"
 
 int main()
 {
-	std::vector<int> test = { 1,3,4,5,6,4,1,0 };
+	std::vector<int> test = { 1,3,3,4,23598235,5,6,4,1,0 };
 	auto test1 = test;
 	sort::makeHeap(test.data(), test.size());
-	sort::bubble(test1);
-	sort::selection(test1);
+	//sort::bubble(test1);
+	//sort::selection(test1);
+	sort::insertion(test1);
 
 	assert(test == test1);
 	
