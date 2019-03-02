8797e8f248da9330622537d78d185073814ee642
cwenhe <wenhe.chen@youhualin.com>
2019-03-02 14:25:19 +0800

    add cmake and adjust directory

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include <vector>
+#include "algorithm/HeapSort.hpp"
+
+int main()
+{
+	std::vector<int> test = { 1,3,4,5,6,4,1,0 };
+	sort::makeHeap(test.data(), test.size());
+	
+
+	return 0;
+}
